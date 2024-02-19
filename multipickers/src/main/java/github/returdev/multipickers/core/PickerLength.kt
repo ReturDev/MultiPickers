a1748bcff4ee92ef69436d933e57c98e579395c3
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2024-02-19 23:38:49 +0100

    Create PickerLength enum class
    - PickerLength represents the length of the visible picker area.
    - The possible values are SHORT and LONG.

---
+++ multipickers/src/main/java/github/returdev/multipickers/core/PickerLength.kt
@@ -0,0 +1,11 @@
+package github.returdev.multipickers.core
+
+/**
+ * Enumeration representing the length of the visible picker area.
+ *
+ * @property value The numeric value associated with the picker length.
+ */
+enum class PickerLength(val value : Int) {
+    SHORT(3),
+    LONG(5)
+}
