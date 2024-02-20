212147f3a3154220c61afe482de1c855adfc7add
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2024-02-20 11:04:46 +0100

    Introduce PickerState class for managing picker state
    - Implement PickerState class responsible for managing the state of a picker.
    - Define properties to track the number of items, selected index, list state, and visibility range.
    - Include functions to update indexes and calculate the index of the selected item in the list.

---
+++ multipickers/src/main/java/github/returdev/multipickers/core/PickerState.kt
@@ -0,0 +1,78 @@
+package github.returdev.multipickers.core
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+
+/**
+ * A stable class that represents the state of a Picker.
+ *
+ * @property itemsCount The total number of items in the Picker.
+ * @param startItemSelectedIndex The initially selected item index. Defaults to 0.
+ * @property pickerLength The length of the visible picker items, either [PickerLength.SHORT] or [PickerLength.LONG]. Defaults to [PickerLength.SHORT].
+ */
+@Stable
+class PickerState(
+    internal val itemsCount: Int,
+    startItemSelectedIndex: Int = 0,
+    val pickerLength: PickerLength = PickerLength.SHORT,
+) {
+
+    // The maximum number of items that can be displayed in the picker.
+    internal val pickerItemsCount = Int.MAX_VALUE
+
+    // The index of the currently selected item.
+    val itemSelectedIndex
+        get() = _selectedIndex % itemsCount
+
+
+    // The index of the picker selected item.
+    private var _selectedIndex by mutableStateOf(
+        calculateSelectedItemListIndex(startItemSelectedIndex)
+    )
+    internal val selectedIndex
+        get() = _selectedIndex
+
+
+    // The state of the lazy list that represents the Picker.
+    val listState: LazyListState = LazyListState(
+        firstVisibleItemIndex = selectedIndex - pickerLength.value / 2
+    )
+
+    // The index of the first visible item in the Picker.
+    private var _firstVisibleIndex by mutableStateOf(
+        listState.firstVisibleItemIndex
+    )
+    internal val firstVisibleIndex : Int
+        get() = _firstVisibleIndex
+
+    // The index of the last visible item in the Picker.
+    private var _lastVisibleIndex by mutableStateOf(
+        firstVisibleIndex + pickerLength.value - 1
+    )
+    internal val lastVisibleIndex : Int
+        get() = _lastVisibleIndex
+
+
+    internal fun updateIndexes(firstVisibleIndex : Int){
+        _firstVisibleIndex = firstVisibleIndex
+        _selectedIndex = firstVisibleIndex + pickerLength.value / 2
+        _lastVisibleIndex = firstVisibleIndex + (pickerLength.value - 1)
+    }
+
+    /**
+     * Calculates the index of the selected item in the list.
+     *
+     * @param itemSelectedIndex The index of the selected item.
+     * @return The calculated index of the selected item in the list.
+     */
+    private fun calculateSelectedItemListIndex(itemSelectedIndex: Int) : Int {
+        val itemsTimesRepeated = pickerItemsCount / itemsCount
+        val startListIndex = (itemsTimesRepeated / 2) * itemsCount
+        return startListIndex + itemSelectedIndex
+    }
+
+}
