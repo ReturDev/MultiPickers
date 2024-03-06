9fbc4784eaa481a7522cabd913d8691a5268e482
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2024-03-06 23:14:13 +0100

    Added Gradle configuration for app publication

--- multipickers/build.gradle.kts
+++ multipickers/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id ("maven-publish")
 }
 
 android {
@@ -41,11 +42,20 @@ android {
 
 dependencies {
 
-    //Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation ("androidx.compose.material3:material3")
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate{
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.returdev"
+                artifactId = "multipickers"
+                version = "1.0.0"
+            }
+        }
+    }
 }
